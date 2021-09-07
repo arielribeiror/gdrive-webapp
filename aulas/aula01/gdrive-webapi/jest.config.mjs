097aa7877ce8671175d3f27942625b94127a5619
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2021-09-07 11:11:42 -0300

    initial adjusts

--- aulas/aula01/gdrive-webapi/jest.config.mjs
+++ aulas/aula01/gdrive-webapi/jest.config.mjs
@@ -1,6 +1,6 @@
 /*
  * For a detailed explanation regarding each configuration property, visit:
- */
+ * */
 
 export default {
   clearMocks: true,
@@ -8,24 +8,17 @@ export default {
   // collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
-  coverageReporters: [
-    "text",
-    "lcov"
-  ],
+  coverageReporters: ["text", "lcov"],
   testEnvironment: "node",
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
-  watchPathIgnorePatterns: [
-    "node_modules"
-  ],
+  watchPathIgnorePatterns: ["node_modules"],
   transformIgnorePatterns: ["node_modules"],
-  collectCoverageFrom: [
-    "src/**/*.js", "!src/**/index.js"
-  ]
+  collectCoverageFrom: ["src/**/*.js", "!src/**/index.js"],
 };
