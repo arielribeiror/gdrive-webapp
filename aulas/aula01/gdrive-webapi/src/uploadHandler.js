64bee9bdf3d8c0e0e25a744a6240cba837eaffb3
Ariel Ribeiro <ariiel.ribeiro@gmail.com>
2021-09-16 22:48:07 -0300

    upload handler more tests added

--- aulas/aula01/gdrive-webapi/src/uploadHandler.js
+++ aulas/aula01/gdrive-webapi/src/uploadHandler.js
@@ -4,13 +4,44 @@ import { pipeline } from "stream/promises";
 import { logger } from "./logger";
 
 export default class UploadHeader {
-  constructor({ io, socketId, downloadsFolder }) {
+  constructor({ io, socketId, downloadsFolder, messsageTimeDelay = 200 }) {
     this.io = io;
     this.socketId = socketId;
     this.downloadsFolder = downloadsFolder;
+    this.ON_UPLOAD_EVENT = "file-upload";
+    this.messsageTimeDelay = messsageTimeDelay;
   }
 
-  handleFileBuffer() {}
+  canExecute(lastExecution) {
+    return Date.now() - lastExecution > this.messsageTimeDelay;
+  }
+
+  handleFileBuffer(filename) {
+    this.lastMessageSent = Date.now();
+    async function* handleData(source) {
+      let processedAlready = 0;
+
+      for await (const chunk of source) {
+        yield chunk;
+
+        processedAlready += chunk.length;
+        if (!this.canExecute(this.lastMessageSent)) {
+          continue;
+        }
+
+        this.lastMessageSent = Date.now();
+        this.io
+          .to(this.socketId)
+          .emit(this.ON_UPLOAD_EVENT, { processedAlready, filename });
+
+        logger.info(
+          `File [${filename}] got ${processedAlready} bytos to ${this.socketId}`
+        );
+      }
+    }
+
+    return handleData.bind(this);
+  }
 
   async onFile(fieldName, file, filename) {
     const saveTo = `${this.downloadsFolder}/${filename}`;
--- aulas/aula01/gdrive-webapi/test/unit/uploadHandler.test.js
+++ aulas/aula01/gdrive-webapi/test/unit/uploadHandler.test.js
@@ -1,6 +1,8 @@
-import { describe, test, expect, jest } from "@jest/globals";
+import { describe, test, expect, beforeEach, jest } from "@jest/globals";
 import fs from "fs";
 import { resolve } from "path";
+import { pipeline } from "stream/promises";
+import { logger } from "../../src/logger";
 import UploadHeader from "../../src/uploadHandler";
 import TestUtil from "../_util/testUtil";
 
@@ -9,6 +11,10 @@ describe("#UploadHandler test suite", () => {
     to: (id) => ioObj,
     emit: (event, message) => {},
   };
+
+  beforeEach(() => {
+    jest.spyOn(logger, "info").mockImplementation();
+  });
 
   describe("#registerEvents", () => {
     test("should call onFile and onFinish functions on Busboy instance", () => {
@@ -80,8 +86,116 @@ describe("#UploadHandler test suite", () => {
       expect(fs.createWriteStream).toHaveBeenCalledWith(expectedFilename);
     });
   });
+
+  describe("#handleFileBuffer", () => {
+    test("should call emit function and it is a transform stream", async () => {
+      jest.spyOn(ioObj, ioObj.to.name);
+      jest.spyOn(ioObj, ioObj.emit.name);
+
+      const handler = new UploadHeader({
+        io: ioObj,
+        socketId: "01",
+      });
+
+      jest.spyOn(handler, handler.canExecute.name).mockReturnValueOnce(true);
+
+      const messages = ["hello"];
+      const source = TestUtil.generateReadableStream(messages);
+      const onWrite = jest.fn();
+      const target = TestUtil.generateWritableStream(onWrite);
+
+      await pipeline(source, handler.handleFileBuffer("filename.txt"), target);
+
+      expect(ioObj.to).toHaveBeenCalledTimes(messages.length);
+      expect(ioObj.emit).toHaveBeenCalledTimes(messages.length);
+
+      expect(onWrite).toBeCalledTimes(messages.length);
+      expect(onWrite.mock.calls.join()).toEqual(messages.join());
+    });
+
+    test("given message timerDelay as 2secs it should emit only two messages during 3 seconds period", async () => {
+      jest.spyOn(ioObj, ioObj.emit.name);
+
+      const day = "2021-07-01 01:01";
+      const onInitVariable = TestUtil.getTimeFromDate(`${day}:01`);
+      const onFirstCanExecute = TestUtil.getTimeFromDate(`${day}:02`);
+      const onSecondCanExecute = TestUtil.getTimeFromDate(`${day}:03`);
+      const onThirdCanExecute = TestUtil.getTimeFromDate(`${day}:04`);
+
+      const onSecondUpdateLastMessageSent = onThirdCanExecute;
+
+      TestUtil.mockDateNow([
+        onInitVariable,
+        onFirstCanExecute,
+        onSecondCanExecute,
+        onThirdCanExecute,
+        onSecondUpdateLastMessageSent,
+      ]);
+
+      const messageTimeDelay = 2000;
+
+      const messages = ["hello", "hello", "world"];
+      const filename = "filename.avi";
+      const expectedMessagesSent = 2;
+
+      const source = TestUtil.generateReadableStream(messages);
+      const handler = new UploadHeader({
+        messageTimeDelay,
+        io: ioObj,
+        socketId: "01",
+      });
+
+      await pipeline(source, handler.handleFileBuffer(filename));
+
+      expect(ioObj.emit).toHaveBeenCalledTimes(expectedMessagesSent);
+
+      const [firstCallResult, secondCallResult] = ioObj.emit.mock.calls;
+      expect(firstCallResult).toEqual([
+        handler.ON_UPLOAD_EVENT,
+        { processedAlready: "hello".length, filename },
+      ]);
+      expect(secondCallResult).toEqual([
+        handler.ON_UPLOAD_EVENT,
+        { processedAlready: "helloworld".length, filename },
+      ]);
+    });
+  });
+
+  describe("#canExecute", () => {
+    test("should return true when time is later than specified delay", () => {
+      const timerDelay = 1000;
+      const uploadHandler = new UploadHeader({
+        io: {},
+        socketId: "",
+        messageTimeDelay: timerDelay,
+      });
+
+      const tickNow = TestUtil.getTimeFromDate("2021-07-01 00:00:03");
+      TestUtil.mockDateNow([tickNow]);
+
+      const lastExecution = TestUtil.getTimeFromDate("2021-07-01 00:00:00");
+
+      const result = uploadHandler.canExecute(lastExecution);
+      expect(result).toBeTruthy();
+    });
+
+    test("should return false when time isnt later than specified delay", () => {
+      const timerDelay = 3000;
+      const uploadHandler = new UploadHeader({
+        io: {},
+        socketId: "",
+        messageTimeDelay: timerDelay,
+      });
+
+      const now = TestUtil.getTimeFromDate("2021-07-01 00:00:02");
+      TestUtil.mockDateNow([now]);
+
+      const lastExecution = TestUtil.getTimeFromDate("2021-07-01 00:00:01");
+
+      const result = uploadHandler.canExecute(lastExecution);
+      expect(result).toBeFalsy();
+    });
+  });
 });
 
-/*
-====================== PAREI NO 01:02:26 AULA 02
-*/
+// PAREI EM 01:50:41 //////////////////////////
